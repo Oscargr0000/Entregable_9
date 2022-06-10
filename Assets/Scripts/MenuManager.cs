f36a1b636746f969b5531a3effabbd9519063200
Oscargr0000 <oscargr0000@gmail.com>
2022-06-10 09:45:03 +0200

    asdasdasd

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -18,9 +18,8 @@ public class MenuManager : MonoBehaviour
     public AudioSource AS;
     public float VolumValue;
 
-    bool blue = false;
-    bool red = false;
-    bool green = false;
+    public GameObject[] Tootips;
+
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +38,8 @@ public class MenuManager : MonoBehaviour
         SliderVolum.value = 0.5f;
         AS.volume = SliderVolum.value;
         PickColor.a = 1;
+
+       
     }
 
 
@@ -50,6 +51,7 @@ public class MenuManager : MonoBehaviour
 
     public void Options()
     {
+        //Activa el canvas de opciones y desactiva los gatos para que estos se actualicen luego
         Canvas[1].SetActive(true);
         Canvas[0].SetActive(false);
 
@@ -58,56 +60,42 @@ public class MenuManager : MonoBehaviour
         CatsImg[2].SetActive(false);
         CatsImg[3].SetActive(false);
         CatsImg[4].SetActive(false);
+
+        Tootips[0].SetActive(false);
+        Tootips[1].SetActive(false);
+        Tootips[2].SetActive(false);
     }
 
     public void Windows()
     {
+        //Cuando se le da al boton regtreso a windows, coge el valor del slider y activa
+        //los gatos de la array y tambien deberia de darle el color seleccionado
         float Cats = CatsNum.value;
 
         for (int i = 0; i <= Cats; i++)
         {
             CatsImg[i].SetActive(true);
-
-            ColorCats[i].color = COLOR;
-
-            //if (green)
-            //{
-                
-            //}
-            //else if(blue)
-            //{
-            //    ColorCats[i].color = Color.blue;
-            //}else if(red)
-            //{
-            //    ColorCats[i].color = Color.red;
-            //}
+            //ColorCats[i].color = COLOR; //NO FUNCIONA BIEN Y NO SE VEN LOS GATOS
         }
         
-
+        //Cambiar de canvas
         Canvas[0].SetActive(true);
         Canvas[1].SetActive(false);
     }
 
+
+    //Botones de seleccion de color
     public void Gr()
     {
         COLOR = ColorsBotton[0].color;
-        //green = true;
-        //blue = false;
-        //red = false;
     }
 
     public void Bl()
     {
         COLOR = ColorsBotton[1].color;
-        //green = false;
-        //blue = true;
-        //red = false;
     }
     public void Re()
     {
         COLOR = ColorsBotton[2].color;
-        //green = false;
-        //blue = false;
-        //red = true;
     }
 }
